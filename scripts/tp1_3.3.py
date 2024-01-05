a84ae2546884d0818b0c73e3f84ebb5bfed4b9cb
GilmarCoutoJr <154843201+GilmarCoutoJr@users.noreply.github.com>
2024-01-04 20:31:04 -0400

    Update tp1_3.3.py

--- scripts/tp1_3.3.py
+++ scripts/tp1_3.3.py
@@ -4,21 +4,21 @@ from banco_de_dados import *
 from dashboard import *
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     start_time = time.time()
     dashboard = Dashboard()
 
     if "--dashboard" in sys.argv:
-        asin = input("Digite o ASIN do produto para iniciar:")
-        n= input("Insira um num para mostrar a evolução diária das médias de avaliação\nao longo do intervalo de tempo coberto no arquivo de entrada:")
-        
-        print("-" * 10 + "DASHBOARD" + "-" * 10)
+        asin = input("Digite o ASIN do produto para iniciar: ")
+        print("\n")
+        print("-" * 25 + "DASHBOARD" + "-" * 25)
         dashboard.verifica_produto(asin)
         dashboard.lista_avaliacao(asin)
         dashboard.lista_maior_similar(asin)
-        dashboard.mostra_evolucao_diaria(asin, n)
+        dashboard.mostra_evolucao_diaria(asin)
         dashboard.lista_produtos_lideres()
         dashboard.lista_produtos_media_util()
         dashboard.lista_categoria_media_avaliacao()
-        print("-" * 10 + "FINISHED" + "-" * 10)
+        dashboard.lista_comentarios_clientes()
 
+        print("-" * 25 + "FINISHED" + "-" * 25)
